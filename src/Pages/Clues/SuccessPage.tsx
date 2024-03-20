bd4fb775cf368d8bee75f2960fb5b060ef186e12
Jenin82 <jenin8282@gmail.com>
2024-03-20 05:57:23 +0530

    feat: structure changes

--- src/Pages/Clues/SuccessPage.tsx
+++ src/Pages/Clues/SuccessPage.tsx
@@ -5,42 +5,48 @@ import styles from "./Clue.module.css";
 import audiofile from "./assets/music7.mp3";
 
 export const SuccessPage = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const fetchUser = async () => {
-    const session = await supabase.auth.getSession();
-    if (session.data.session?.user.id) {
-      let { data: clue, error } = await supabase
-        .from("clue")
-        .select("*")
-        // Filters
-        .eq("id", session.data.session.user.id);
-      if (error) {
-        throw error.message;
-      }
-      const progress = clue?.[0]?.progress;
-      if (progress < 7) {
-        console.log(progress);
-        navigate("/2563");
-        return;
-      }
-    }
-  };
+	const fetchUser = async () => {
+		const session = await supabase.auth.getSession();
+		if (session.data.session?.user.id) {
+			let { data: clue, error } = await supabase
+				.from("clue")
+				.select("*")
+				// Filters
+				.eq("id", session.data.session.user.id);
+			if (error) {
+				throw error.message;
+			}
+			if (clue?.length === 0) {
+				navigate("/2563");
+				return;
+			}
+			const progress: number = clue?.[0]?.progress || 0;
+			if (progress < 7) {
+				console.log(progress);
+				navigate("/2563");
+				return;
+			}
+		}
+	};
 
-  useEffect(() => {
-    fetchUser();
-    // Audio setup
-    const audio = new Audio(audiofile);
-    audio.loop = true;
-    audio.play().catch((error) => console.error("Audio play failed:", error));
+	useEffect(() => {
+		fetchUser();
+		// Audio setup
+		const audio = new Audio(audiofile);
+		audio.loop = true;
+		audio
+			.play()
+			.catch((error) => console.error("Audio play failed:", error));
 
-    // Cleanup function to stop music when component unmounts
-    return () => audio.pause();
-  }, []);
+		// Cleanup function to stop music when component unmounts
+		return () => audio.pause();
+	}, []);
 
-  return (
+	return (
 		<div className={styles.Clue7}>
 			<a href="tel:+91 79091 37548">Call</a>
 		</div>
-  );
+	);
 };
